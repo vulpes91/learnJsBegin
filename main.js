057f333618b3e8ef5317982ae5bedfdc15f95f94
Vulpes91 <40720021+vulpes91@users.noreply.github.com>
2021-03-09 17:11:45 +0300

    начинаю изучать js

---
+++ main.js
@@ -0,0 +1,54 @@
+// 1 Переменные
+
+//старый вариант
+// var name = 'Alexander'
+
+// //новый вариант
+// const sex = 'Male'
+// const myName = 'Alexandr' //string
+// //let age = 29 //number
+// const isCoder = true // boolean
+// const monkeyCode = 'Monkey is coding.'
+
+
+//использовать const преимущественно, если не подразумевается изменение переменной 
+// JS позволяет писать без ; однако в некоторых моментах она обязательна 
+
+
+
+// 2 JS и типы данных  (мутирование)
+// console.log('Hi! My name is ' + myName + ' and my age is ' + age + '. What is happening? ' + monkeyCode)
+// //alert('Работает только в браузере')
+// //свойтсво alert будет работать только в браузере
+
+// const insertAge = prompt('Введите ваш возраст')
+// alert('Ваш возраст равен ' + insertAge)
+// alert('Спасибо за ответ')
+//данный код попросит ввести (prompt) возраст и выдаст сообщение с тем значением, которое ввёл пользователь
+
+// 3 Операторы 
+// let currentYear = 2020
+// const birthYear = 1991
+// const age = currentYear - birthYear
+// //console.log('Ваш возраст: ' + age + ' лет')
+
+// const a = 10
+// const b = 5
+// let c = 32
+// //c = c + a
+// c += a
+// c += a аналогично записи c = c + a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear)
+// console.log(--currentYear)
+
+
+console.log(c)
+
+//4 Типы данных 
+
+
